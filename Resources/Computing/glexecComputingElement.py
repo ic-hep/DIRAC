e578f1275120026784e3a83ec4e1162e1aaf3f27
Alexander Richards <a.richards@imperial.ac.uk>
2015-10-01 16:01:26 +0200

    Fix python location problem under Glexec
    This patch fixes the problem that when running under glexec the job has no access to the pilot python install. To do this we re-install the correct version of DIRAC under glexec and source the DIRAC bashrc.

--- Resources/Computing/glexecComputingElement.py
+++ Resources/Computing/glexecComputingElement.py
@@ -5,12 +5,16 @@
 __RCSID__ = "$Id$"
 
 import os
+import re
 import stat
+import uuid
 import distutils.spawn
+from tempfile import mkdtemp
+from shutil import rmtree
 
 import DIRAC
 
-from DIRAC                                                  import S_OK, S_ERROR
+from DIRAC                                                  import S_OK, S_ERROR, gConfig
 
 from DIRAC.Resources.Computing.ComputingElement             import ComputingElement
 from DIRAC.Core.Utilities.ThreadScheduler                   import gThreadScheduler
@@ -19,6 +23,12 @@ from DIRAC.Core.Utilities.Subprocess                        import shellCall
 
 
 MandatoryParameters = [ ]
+
+def S_RETRYERROR(computing_element, *args, **kwargs):
+    ret = S_ERROR(*args, **kwargs)
+    if computing_element.ceParameters.get('RescheduleOnError', False):
+      ret['ReschedulePayload'] = True
+    return ret
 
 class glexecComputingElement( ComputingElement ):
 
@@ -43,6 +53,9 @@ class glexecComputingElement( ComputingElement ):
   def submitJob( self, executableFile, proxy, dummy = None ):
     """ Method to submit job, should be overridden in sub-class.
     """
+    self.log.notice( 'Starting glexec submission, reschedule on error: %s' % \
+                        self.ceParameters.get('RescheduleOnError', False))
+    
     self.log.verbose( 'Setting up proxy for payload' )
     result = self.writeProxyToFile( proxy )
     if not result['OK']:
@@ -51,7 +64,7 @@ class glexecComputingElement( ComputingElement ):
     payloadProxy = result['Value']
     if not os.environ.has_key( 'X509_USER_PROXY' ):
       self.log.error( 'X509_USER_PROXY variable for pilot proxy not found in local environment' )
-      return S_ERROR( 'X509_USER_PROXY not found' )
+      return S_RETRYERROR(self, 'X509_USER_PROXY not found' )
 
     pilotProxy = os.environ['X509_USER_PROXY']
     self.log.info( 'Pilot proxy X509_USER_PROXY=%s' % pilotProxy )
@@ -66,25 +79,47 @@ class glexecComputingElement( ComputingElement ):
     result = self.glexecLocate()
     if result['OK']:
       glexecLocation = result['Value']
-      self.log.info( 'glexec found for local site at %s' % glexecLocation )
+      self.log.notice( 'glexec found for local site at %s' % glexecLocation )
 
     if glexecLocation:
       result = self.recursivelyChangePermissions()
       if not result['OK']:
         self.log.error( 'Permissions change failed, continuing regardless...' )
     else:
-      self.log.info( 'glexec not found, no permissions to change' )
+      self.log.error( 'glexec not found' )
+      return S_RETRYERROR(self, "Could not find glexec")
+
+    # Determine if the running directory is executable by the user, if not then run in home
+    pilot_dir = os.getcwd()
+    job_dir = os.path.join('~', 'job_%s' % uuid.uuid4())
+    running_in_home = False
+    path = pilot_dir
+    while path != '/':  # we will run in the home dir if no route to the pilot_dir
+      if not os.stat(path).st_mode & stat.S_IXOTH:
+        self.log.notice("Switching to home dir as current path '%s' contains element '%s' which is not executable by other users" % (pilot_dir, path))
+        running_in_home = True
+        result = self.glexecMakeDir(glexecLocation, job_dir)
+        if not result['OK']:
+            self.log.error("Failed to create job dir")  
+            return S_RETRYERROR(self, "couldn't setup the user job dir inside their home dir")
+        break
+      path = os.path.dirname(path)
+
+    # if running dir is executable by user then create a Dirac install dir and job dir
+    if not running_in_home:
+      result = self.setupPilotDir(glexecLocation, pilot_dir)
+      if not result['OK']:
+        self.log.error("Submission aborted as no pilot and job dir setup.")
+        return result
+      pilot_dir, job_dir = result['Value']
 
     #Test glexec with payload proxy prior to submitting the job
-    result = self.glexecTest( glexecLocation )
+    self.log.notice("Running in job_dir: %s"% job_dir)
+    result = self.glexecTest( glexecLocation, job_dir )
     if not result['OK']:
       res = self.analyseExitCode( result['Value'] ) #take no action as we currently default to InProcess
-      glexecLocation = None
-      if 'RescheduleOnError' in self.ceParameters and self.ceParameters['RescheduleOnError']:
-        result = S_ERROR( 'gLexec Test Failed: %s' % res['Value'] )
-        result['ReschedulePayload'] = True
-        return result
-      self.log.info( 'glexec test failed, will submit payload regardless...' )
+      self.log.error( 'glexec test failed...' )
+      return S_RETRYERROR(self, 'gLexec Test Failed: %s' % res['Value'] )
 
     #Revert to InProcess behaviour
     if not glexecLocation:
@@ -96,6 +131,37 @@ class glexecComputingElement( ComputingElement ):
                                       taskArgs = ( glexecLocation, pilotProxy, payloadProxy ),
                                       executions = 0, elapsedTime = 0 )
 
+    # read in old exe file and make necessary path modifications
+    old_file = ''
+    with open(executableFile, 'rb') as exec_file:
+      for line in re.findall(r'^(?![\s]*$)([^#\n]*).*$', exec_file.read(), flags=re.MULTILINE):
+        match = re.match(r'^[^ ]*/*python[\d.]* +([^ ]*/job/Wrapper/Wrapper_[\d.]+) ', line)
+        if match is not None:
+          wrapper_file = match.groups()[0]
+          self.glexecCopyFile(glexecLocation,
+                              infile=wrapper_file,
+                              outfile=os.path.join(job_dir, os.path.basename(wrapper_file)))
+        old_file += re.sub(r'^[^ ]*/*python[\d.]* +[^ ]*/job/Wrapper/(Wrapper_[\d.]+) ', r'python \1 ', line) + '\n'
+
+    # write the new exe script
+    with open(executableFile, 'wb') as new_file:
+      new_script = \
+"""#!/bin/bash
+set -e
+cd {jobDir!s}
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+python dirac-pilot.py -S {diracSetup!s} -r {diracVersion!s} -X InstallDIRAC,ConfigureBasics -C {diracConfigServer!s}
+source bashrc
+dirac-proxy-info
+{oldFile!s}
+""".format(jobDir=job_dir,
+           diracSetup=gConfig.getValue( 'DIRAC/Setup' ),
+           diracVersion=DIRAC.version,
+           diracConfigServer=re.sub(' *, *', ',', gConfig.getValue('/DIRAC/Configuration/Servers').strip(', ')),
+           oldFile=old_file)
+      self.log.debug("Using script:\n %s" % new_script)
+      new_file.write(new_script)
+
     #Submit job
     self.log.info( 'Changing permissions of executable to 0755' )
     try:
@@ -104,11 +170,35 @@ class glexecComputingElement( ComputingElement ):
       self.log.error( 'Failed to change permissions of executable to 0755 with exception', 
                       '\n%s' % ( x ) )
 
-    result = self.glexecExecute( os.path.abspath( executableFile ), glexecLocation )
+    # push files through to the user job dir
+    glexec_executableFile = os.path.join(job_dir, os.path.basename(executableFile))
+    self.glexecCopyFile(glexecLocation,
+                        infile=os.path.abspath(executableFile),
+                        outfile=glexec_executableFile,
+                        executable=True)
+    self.glexecCopyFile(glexecLocation,
+                        infile=os.path.join(DIRAC.rootPath, 'DIRAC/Core/scripts/dirac-install.py'),
+                        outfile=os.path.join(job_dir, 'dirac-install.py'))
+    self.glexecCopyFile(glexecLocation,
+                        infile=os.path.join(DIRAC.rootPath, 'DIRAC/WorkloadManagementSystem/PilotAgent/dirac-pilot.py'),
+                        outfile=os.path.join(job_dir, 'dirac-pilot.py'))
+    self.glexecCopyFile(glexecLocation,
+                        infile=os.path.join(DIRAC.rootPath, 'DIRAC/WorkloadManagementSystem/PilotAgent/pilotTools.py'),
+                        outfile=os.path.join(job_dir, 'pilotTools.py'))
+    self.glexecCopyFile(glexecLocation,
+                        infile=os.path.join(DIRAC.rootPath, 'DIRAC/WorkloadManagementSystem/PilotAgent/pilotCommands.py'),
+                        outfile=os.path.join(job_dir, 'pilotCommands.py'))
+
+    result = self.glexecExecute( glexec_executableFile, glexecLocation )
     if not result['OK']:
       self.analyseExitCode( result['Value'] ) #take no action as we currently default to InProcess
       self.log.error( 'Failed glexecExecute', result )
       return result
+
+    if not running_in_home:
+      result = self.removePilotDir(glexecLocation, pilot_dir, job_dir)
+      if not result['OK']:
+        self.log.error("Failed to clean up the job dir: %s" % job_dir)
 
     self.log.debug( 'glexec CE result OK' )
     self.submittedJobs += 1
@@ -235,7 +325,7 @@ class glexecComputingElement( ComputingElement ):
     return S_OK( error )
 
   #############################################################################
-  def glexecTest( self, glexecLocation ):
+  def glexecTest( self, glexecLocation, job_dir='~'):
     """Ensure that the current DIRAC distribution is group readable e.g. dirac-proxy-info
        also check the status code of the glexec call.
     """
@@ -247,7 +337,6 @@ class glexecComputingElement( ComputingElement ):
     cmds.append( 'id' )
     cmds.append( 'hostname' )
     cmds.append( 'date' )
-    cmds.append( '%s/scripts/dirac-proxy-info' % DIRAC.rootPath )
     fopen = open( testFile, 'w' )
     fopen.write( '\n'.join( cmds ) )
     fopen.close()
@@ -257,9 +346,76 @@ class glexecComputingElement( ComputingElement ):
     except Exception, x:
       self.log.error( 'Failed to change permissions of test script to 0755 with exception', 
                       '\n%s' % ( x ) )
-      return S_ERROR( 'Could not change permissions of test script' )
+      return S_RETRYERROR(self, 'Could not change permissions of test script' )
 
-    return self.glexecExecute( os.path.abspath( testFile ), glexecLocation )
+    glexec_executableFile = os.path.join(job_dir, testFile)
+    self.glexecCopyFile(glexecLocation,
+                        infile=os.path.abspath(testFile),
+                        outfile=glexec_executableFile,
+                        executable=True)
+
+    return self.glexecExecute( glexec_executableFile, glexecLocation )
+
+  #############################################################################
+  def setupPilotDir(self, glexecLocation, pilot_dir):
+    """Setup a temporary pilot dir with a job dir inside for the dirac install"""
+    if not glexecLocation:
+      return S_RETRYERROR(self, "Can't setup pilot dir as can't find glexec")
+    pilot_dir = mkdtemp(prefix='glexec_pilot', dir=pilot_dir)
+    job_dir = os.path.join(pilot_dir, 'job_dir')
+    os.chdir(pilot_dir)
+    os.chmod(pilot_dir, 0o1777)  # sticky bit stops the inner dir being overwritten by another user before we chmod again
+    result = self.glexecMakeDir(glexecLocation, job_dir)
+    os.chmod(pilot_dir, 0o0711)
+    if not result['OK']:
+      self.log.error("Failed to create job dir as user")
+      return S_RETRYERROR(self, "Failed to create job dir with user credentials")
+    return S_OK((pilot_dir, job_dir))
+
+  #############################################################################
+  def removePilotDir(self, glexecLocation, pilot_dir, job_dir):
+    """Remove the directory structures created in setupPilotDir"""
+    os.chmod(pilot_dir, 0o1777)
+    cmd = "%s /bin/bash -c '/bin/rm -rf %s'" % (glexecLocation, job_dir)
+    result = shellCall( 0, cmd, callbackFunction = self.sendOutput )
+    os.chmod(pilot_dir, 0o0711)
+    if not result['OK']:
+      self.log.error("Failed to call: glexec /bin/rm")
+      return S_RETRYERROR(self, "Failed to delete job dir")
+
+    try:
+      rmtree(pilot_dir)
+    except Exception, x:
+      self.log.error("Failed to clean up pilot dir: %s" % pilot_dir)
+      return S_RETRYERROR(self, "Failed to remove the pilot dir")
+
+    return S_OK()
+
+  #############################################################################
+  def glexecMakeDir(self, glexecLocation, path):
+    """Create new directories with the users credentials"""
+    self.log.notice("using glexec to make: %s" % path)
+    cmd = "%s /bin/bash -c '/bin/mkdir -p %s'" % (glexecLocation, path)
+    result = shellCall( 0, cmd, callbackFunction = self.sendOutput )
+    if not result['OK']:
+      self.log.error("Failed to create dir: %s with '%s'" % (path, cmd))
+      return result
+    return S_OK()
+
+  #############################################################################
+  def glexecCopyFile(self, glexecLocation, infile, outfile=None, executable=False):
+    """Copy files from one location with the pilots credentials to another with the users credentials"""
+    executemod = ''
+    if executable:
+      executemod = "; chmod 700 %s" % outfile
+    if outfile is None:
+      outfile = os.path.basename(infile)
+    cmd = "/bin/cat %s | %s /bin/bash -c '/bin/cat > %s%s'" % (infile, glexecLocation, outfile, executemod)    
+    result = shellCall( 0, cmd, callbackFunction = self.sendOutput )
+    if not result['OK']:
+      self.log.error("Failed to copy file through glexec")
+      return S_RETRYERROR(self, "Failed to copy file")
+    return S_OK()
 
   #############################################################################
   def glexecExecute( self, executableFile, glexecLocation ):
@@ -283,7 +439,7 @@ class glexecComputingElement( ComputingElement ):
     stdError = resultTuple[2]
     self.log.info( "Status after the glexec execution is %s" % str( status ) )
     if status >=127:
-      error = S_ERROR( status )
+      error = S_RETRYERROR(self, status )
       error['Value'] = ( status, stdOutput, stdError )
       return error
 
@@ -301,12 +457,12 @@ class glexecComputingElement( ComputingElement ):
     else: #try to locate the excutable in the PATH
       glexecPath = distutils.spawn.find_executable( "glexec" )
     if not glexecPath:
-      self.log.info( 'Unable to locate glexec, site does not have GLITE_LOCATION nor OSG_GLEXEC_LOCATION defined' )
-      return S_ERROR( 'glexec not found' )
+      self.log.error( 'Unable to locate glexec, site does not have GLITE_LOCATION nor OSG_GLEXEC_LOCATION defined' )
+      return S_RETRYERROR(self, 'glexec not found' )
 
     if not os.path.exists( glexecPath ):
-      self.log.info( 'glexec not found at path %s' % ( glexecPath ) )
-      return S_ERROR( 'glexec not found' )
+      self.log.error( 'glexec not found at path %s' % ( glexecPath ) )
+      return S_RETRYERROR(self, 'glexec not found' )
 
     return S_OK( glexecPath )
 
