4b68158b9f253bf3156f05e28ae8350996e30f68
Alexander Richards <a.richards@imperial.ac.uk>
2015-11-05 11:47:44 +0000

    Adding glexec computing element with inprocess fallback
    Create a simple wrapper class that runs first glexecComputingElement.submitJob and then falls back in the case of error to run InProcessComputingElement.submitJob. NOTE that monitorProxy method needs to also deligate to the correct base class.

---
+++ Resources/Computing/glexecInProcessComputingElement.py
@@ -0,0 +1,31 @@
+from DIRAC import S_OK, S_ERROR
+from DIRAC.Resources.Computing.InProcessComputingElement import InProcessComputingElement
+from DIRAC.Resources.Computing.glexecComputingElement import glexecComputingElement
+
+
+class glexecInProcessComputingElement(glexecComputingElement, InProcessComputingElement):
+    def __init__(self, ceUniqueID):
+        super(glexecInProcessComputingElement, self).__init__(ceUniqueID)
+        self.in_process = False
+
+    def submitJob(self, executableFile, proxy, dummy=None):
+        try:
+            result = glexecComputingElement.submitJob(self, executableFile, proxy, dummy)
+        except Exception as e:
+            self.log.exception(e)
+            result = S_ERROR(str(e))
+
+        if not result['OK']:
+            self.log.error("Failed to submit job using glexecComputingElement: %s" % result['Message'])
+            self.log.notice("faling back to InProcessComputingElement...")
+            self.in_process = True
+            result = InProcessComputingElement.submitJob(self, executableFile, proxy, dummy)
+            if not result['OK']:
+                self.log.error("Failed to submit job using InProcessComputingElement as fallback: %s" % result['Message'])
+                return result
+        return S_OK()
+
+    def monitorProxy(self, *args, **kwargs):
+        if self.in_process:
+            return InProcessComputingElement.monitorProxy(self, *args, **kwargs)
+        return glexecComputingElement.monitorProxy(self, *args, **kwargs)
